aae1ecb7c87f93404d5b5a3a13100497302a83f7
Larskan <101105018+Larskan@users.noreply.github.com>
2023-05-10 05:58:36 +0200

    Add project files.

---
+++ WebApplication1WebHook/GenericJsonWebHookHandler.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNet.WebHooks;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApplication1WebHook
+{
+    public class GenericJsonWebHookHandler : WebHookHandler
+    {
+        public GenericJsonWebHookHandler()
+        {
+            this.Receiver = "genericjson";
+        }
+
+        public override Task ExecuteAsync(string receiver, WebHookHandlerContext context)
+        {
+            System.Diagnostics.Debug.WriteLine("Called1-----------------------");
+            
+
+            // Get JSON from WebHook
+            JObject data = context.GetDataOrDefault<JObject>();
+
+            System.Diagnostics.Debug.WriteLine("test: "  + data.ToString());
+
+            System.Diagnostics.Debug.WriteLine("Called2-----------------------");
+
+            //if (context.Id == "i")
+            //{
+            //    //You can use the passed in Id to route differently depending on source.
+            //}
+            //else if (context.Id == "z")
+            //{
+            //}
+
+            string action = context.Actions.FirstOrDefault();
+
+            return Task.FromResult(true);
+        }
+    }
+}
