31c8e22d14c48c250c8262c3cf32307a4025871a
Larskan <101105018+Larskan@users.noreply.github.com>
2023-05-13 14:18:09 +0200

    update

--- WebApplication1WebHook/DynamicsFacade.cs
+++ WebApplication1WebHook/DynamicsFacade.cs
@@ -17,7 +17,7 @@ namespace WebApplication1WebHook
         private string Login = $"admin:Password";
 
         //The IP for docker
-        private string dockerIP = "172.25.161.237:7047";
+        private string dockerIP = "172.25.169.148:7049";
 
         //The name of container, if it doesnt work, use dockerIP
         private string dockerContainer = "bc-container";
@@ -128,10 +128,74 @@ namespace WebApplication1WebHook
 
         public async Task CreateItem(JObject jObject)
         {
+            var _token = Login;
+            var _tokenBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(_token));
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //Token to authenticate request, to be added to HTTP request header
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _tokenBase64);
+
+            Payload payload = new Payload() { payloadinfo = jObject.ToString() };
+            String jsonData = JsonConvert.SerializeObject(payload);
+            System.Diagnostics.Debug.WriteLine(jsonData);
+
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            //Name of the service attached to your Web Service(The name you gave your Codeunit in BC365 Web Service)
+            var serviceName = "WooIn";
+            //Name of the procedure you wish to call based on your Service Name
+            var procedureName = "ProcessCreateItem";
+
+            HttpResponseMessage response = await client.PostAsync("http://" + this.dockerIP + "/BC/ODataV4/" + serviceName + "_" + procedureName + "?company=CRONUS%20Danmark%20A%2FS", content);
+            string data = "";
+
+            if (response.IsSuccessStatusCode)
+            {
+                data = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine("Response: " + response);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Error");
+            }
+            System.Diagnostics.Debug.WriteLine("Result: " + data);
 
         }
         public async Task UpdateItemStock(JObject jObject)
         {
+            var _token = Login;
+            var _tokenBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(_token));
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            //Token to authenticate request, to be added to HTTP request header
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", _tokenBase64);
+
+            Payload payload = new Payload() { payloadinfo = jObject.ToString() };
+            String jsonData = JsonConvert.SerializeObject(payload);
+            System.Diagnostics.Debug.WriteLine(jsonData);
+
+            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            //Name of the service attached to your Web Service(The name you gave your Codeunit in BC365 Web Service)
+            var serviceName = "WooIn";
+            //Name of the procedure you wish to call based on your Service Name
+            var procedureName = "ProcessUpdateStock";
+
+            HttpResponseMessage response = await client.PostAsync("http://" + this.dockerIP + "/BC/ODataV4/" + serviceName + "_" + procedureName + "?company=CRONUS%20Danmark%20A%2FS", content);
+            string data = "";
+
+            if (response.IsSuccessStatusCode)
+            {
+                data = await response.Content.ReadAsStringAsync();
+                System.Diagnostics.Debug.WriteLine("Response: " + response);
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Error");
+            }
+            System.Diagnostics.Debug.WriteLine("Result: " + data);
 
         }
 
