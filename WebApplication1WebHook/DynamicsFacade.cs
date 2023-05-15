34d45bd8a11044546a34487e45c4eb28c6bf5ef1
Larskan <101105018+Larskan@users.noreply.github.com>
2023-05-15 08:36:18 +0200

    update

--- WebApplication1WebHook/DynamicsFacade.cs
+++ WebApplication1WebHook/DynamicsFacade.cs
@@ -29,14 +29,12 @@ namespace WebApplication1WebHook
             Payload payload = new Payload() { payload = jObject.ToString() };
             System.Diagnostics.Debug.WriteLine("Payload: " + jObject);
             String jsonData = JsonConvert.SerializeObject(payload);
-            System.Diagnostics.Debug.WriteLine("Payload: "+ jsonData);
             var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-            System.Diagnostics.Debug.WriteLine("Payload: " + content);
 
             //Name of the service attached to your Web Service(The name you gave your Codeunit in BC365 Web Service)
             var serviceName = "FromWoo"; 
             //Name of the procedure you wish to call based on your Service Name
-            //
+      
             var procedureName = "ProcessCreateSalesOrder"; 
             HttpResponseMessage response = await client.PostAsync("http://" + this.dockerIP + "/BC/ODataV4/"+ serviceName + "_" + procedureName + "?company=CRONUS%20Danmark%20A%2FS", content);
             
@@ -84,7 +82,6 @@ namespace WebApplication1WebHook
             System.Diagnostics.Debug.WriteLine("Result: " + data);
         }
     }
-
     class Payload
     {
         [JsonProperty("payload")]
